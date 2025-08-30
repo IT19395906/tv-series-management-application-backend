217b33bf3418f049fa22c0bd8a4213191e778604
IT19395906 <IT19395906@my.sliit.lk>
2025-08-31 00:34:48 +0530

    get categories and languages as array from API

--- src/main/java/com/tvseries/TvSeriesManagementSystemBackend/controller/TvSeriesController.java
+++ src/main/java/com/tvseries/TvSeriesManagementSystemBackend/controller/TvSeriesController.java
@@ -1,5 +1,6 @@
 package com.tvseries.TvSeriesManagementSystemBackend.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,10 @@ import com.tvseries.TvSeriesManagementSystemBackend.dto.SearchDto;
 import com.tvseries.TvSeriesManagementSystemBackend.dto.SubmitDto;
 import com.tvseries.TvSeriesManagementSystemBackend.entity.TvSeries;
 import com.tvseries.TvSeriesManagementSystemBackend.service.TvSeriesService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@RequestMapping("/tvseries")
+@RequestMapping("/api/tvseries")
 @CrossOrigin(origins = "*")
 public class TvSeriesController {
 
@@ -24,8 +26,21 @@ public class TvSeriesController {
     private TvSeriesService service;
 
     @GetMapping("")
-     public String home() {
+    public String home() {
         return "controller work";
+    }
+
+    @GetMapping("categories")
+    public List<String> getAllCategories() {
+        return Arrays.asList("Action", "Comedy", "Drama", "Thriller", "Horror", "Adventure", "Crime",
+                "Romance", "Documentary", "Sport", "Mystery", "Musical", "History", "Fantasy", "Biography",
+                "Animation");
+    }
+
+    @GetMapping("languages")
+    public List<String> getAllLanguages() {
+        return Arrays.asList("English", "Hindi", "Spanish", "French", "German", "Chinese", "Japanese",
+                "Korean", "Arabic", "Portuguese", "Russian", "Italian", "Turkish", "Dutch");
     }
 
     @PostMapping("add")
